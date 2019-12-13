dc403f5f3bb5f30a39d018f3732a5e117b5a6944
MherTemuryan <mher.temuryan@gmail.com>
2019-12-13 22:21:44 +0400

    copied header component

---
+++ src/components/header copy/MyCreate/myCreates.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./my_creates.css";
+
+
+function MyCreate() {
+  return (
+    <div className="settings">
+      myCreates
+    </div>
+  );
+}
+
+export default MyCreate;
--- src/components/user/index.js
+++ src/components/user/index.js
@@ -2,39 +2,11 @@ import React from "react";
 import Sidebar from "./Sidebar/Sidebar"
 import UserInformation from "./userInformation/userInfrmation";
 
-export default class User extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            profileOptions: "",
-        }
-    }
-    editProfileHandelClick = (v)=> {
-        console.log(v);
-        console.log(v.target);
-        console.log(v.target.value);
-        // switch (v) {
-        //     case "editPr":
-        //         this.setState({
-        //             profileOptions: 1,
-        //         });
-        //         break;
-        //         case "accountSt":
-        //         this.setState({
-        //             profileOptions: 2,
-        //         });
-        //         break;
-        // }
-    };
-
-
-    render() {
-        return (
-            <div>
-                <Sidebar />
-                <UserInformation />
-            </div>
-            )
-    }
-
+export default function User() {
+    return (
+        <div>
+            <Sidebar />
+            <UserInformation/>
+        </div>
+    );
 }
