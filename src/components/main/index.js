ece450fc74892f72b789a3dbd8ffc23844436554
pinterestshavo <shavarsh.abrahamyan1993@gmail.com>
2019-12-23 21:13:27 +0400

    fixed changes after merging

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,18 +1,11 @@
 import React, {useState} from "react"
 import Login from '../login/index';
 
-export default function Main() {
+export default function Main(props) {
 
     return (
-<<<<<<< HEAD
-        <div >
-            Hello!
-        </div>
-=======
         <main>
             {props.children}
         </main>
-
->>>>>>> cc89f598f2bfc9a0f962d17ec0ec02b1fcbb3825
     )
 };
